3d87f22348c613dc95aa22b15cce382fe919a039
honestyyaaboatemaa57-blip <honestyyaaboatemaa57@gmail.com>
2026-05-31 11:39:51 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,43 @@
+File: initials.py
+# Description: Print out stylized large initials for Honesty Yaa Boatemaa.
+# Assignment Number: 1
+# 
+# Name: Honesty Yaa Boatemaa
+# STUDENT ID: 2425402337
+# Email: 2425402337@live.gctu.edu.gh
+# Grader: Augustus Buckman
+# On my honor, Honesty Yaa Boatemaa, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
   pass #code goes here
+# Print the student's stylized large initials using only print() and strings.
+    
+    # Blank line before small initials [cite: 20]
+    print()
+    
+    # Small initials [cite: 8]
+    print("...HYB")
+    
+    # Blank line between small and large initials [cite: 20]
+    print()
+    
+    # Large initials, 10 lines high and 12 characters wide each [cite: 12, 17]
+    # Each followed by a large period made of 4 asterisks [cite: 14]
+    # Three periods columns to the left and right of each letter/period [cite: 15]
+    print("...HHHH....HHHH........YYYY....YYYY........BBBBBBBBBBB.....")
+    print("...HHHH....HHHH.........YYYY..YYYY.........BBBBBBBBBBBB.....")
+    print("...HHHH....HHHH..........YYYYYYYY..........BBB.......BB.....")
+    print("...HHHH....HHHH...........YYYYYY...........BBB.......BB.....")
+    print("...HHHHHHHHHHHH............YYYY............BBBBBBBBBBBB.....")
+    print("...HHHHHHHHHHHH............YYYY............BBBBBBBBBBBB.....")
+    print("...HHHH....HHHH............YYYY............BBB.......BB.....")
+    print("...HHHH....HHHH............YYYY............BBB.......BB.....")
+    print("...HHHH....HHHH...**.......YYYY.......**...BBBBBBBBBBBB...**")
+    print("...HHHH....HHHH...**.......YYYY.......**...BBBBBBBBBBB....**")
+
+    print()
+
 
 
 
